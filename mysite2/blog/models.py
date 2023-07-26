664792aad152352e9dd748e39a4f649a051fd675
Hemin Jeong <jhemin0415@gmail.com>
2023-07-26 10:17:23 +0900

    인덱싱 메타, 스테이터스 틀래스 추가

--- mysite2/blog/models.py
+++ mysite2/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+
+class Post(models.Model):
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.TextField(max_length=100)
+    slug = models.SlugField(allow_unicode=True)
+    body = models.TextField(max_length=300)
+    publish = models.DateTimeField(default=None)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT
+    )
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+
+    def __str__(self):
+        return self.title
+
