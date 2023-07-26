03fce8f469a93a5a1f4859817b66087da7c3302c
Hemin Jeong <jhemin0415@gmail.com>
2023-07-26 15:50:40 +0900

    페이지네이션 추가

--- mysite2/blog/views.py
+++ mysite2/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView, ListView
+from django.core.paginator import Paginator
 from .models import *
 
 # Create your views here.
@@ -9,8 +10,11 @@ class PostList(ListView):
     queryset = Post.objects.all()
     context_object_name = 'posts'
     template_name = 'post_list.html'
+    paginate_by = 4
+    page_kwarg = 'page'
 
 class PostDetail(DetailView):
     model = Post
     context_object_name = 'post'
     template_name = 'post_detail.html'
+
