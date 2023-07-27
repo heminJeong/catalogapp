6f60b4202609e998dce68952cbc2f5c108fbf0ee
Hemin Jeong <jhemin0415@gmail.com>
2023-07-27 11:20:29 +0900

    코멘트 모델, 어드민 생성 url 끝부분 / 추가

--- mysite2/blog/admin.py
+++ mysite2/blog/admin.py
@@ -5,3 +5,7 @@ from .models import *
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'body', 'publish', 'created', 'updated', 'status']
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['post', 'name', 'email', 'body', 'created', 'updated', 'active']
+    list_filter = ['active, created, updated']
--- mysite2/blog/urls.py
+++ mysite2/blog/urls.py
@@ -5,6 +5,6 @@ app_name='blog'
 
 urlpatterns = [
     path('', PostList.as_view(), name='post_list'),
-    path('<int:pk>', PostDetail.as_view(), name='post_detail'),
-    path('<int:pk>/share', PostShare.as_view(), name='post_share'),
+    path('<int:pk>/', PostDetail.as_view(), name='post_detail'),
+    path('<int:pk>/share/', PostShare.as_view(), name='post_share'),
 ]
